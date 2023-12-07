e2f1f02a74fe6a9b5d5421c5a45e1de95fa2ebf1
ramune314159265 <ramune314159254@gmail.com>
2023-12-07 23:54:00 +0900

    地震情報追加(作業中)

---
+++ src/util/earthquake.js
@@ -0,0 +1,12 @@
+const quakeScales = {
+    '-1': {
+        name: '不明',
+        level: -1,
+        anciColor: ''
+    },
+    '10': {
+        name: '1',
+        level: 1,
+        anciColor: '[30m',
+    }
+}
