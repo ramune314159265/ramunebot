0a604170c9d127d23062a77583488f768107a797
BuildTools <unconfigured@null.spigotmc.org>
2023-12-08 19:40:58 +0900

    util/earthquakeを使うように

--- src/interactions/commands/earthquake/index.js
+++ src/interactions/commands/earthquake/index.js
@@ -1,5 +1,8 @@
 const { EmbedBuilder } = require('discord.js');
 const fetch = require('node-fetch');
+
+const { quakeScales } = require('../../../util/earthquake')
+
 module.exports = {
   data: {
     name: "earthquake",
@@ -7,14 +10,9 @@ module.exports = {
   },
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true })
-    const rawQuake = await (await fetch('https://api.p2pquake.net/v2/jma/quake?limit=40')).json()
-    /**
-     * @see https://gist.github.com/sevenc-nanashi/67bfed2bdd0758eb20ac9bcd6fd88f84
-     */
-    const scale = [];
+    const rawQuakeInfo = await (await fetch('https://api.p2pquake.net/v2/jma/quake?limit=40')).json()
 
-    [scale[10], scale[20], scale[30], scale[40], scale[45], scale[50], scale[55], scale[60], scale[70]] = ['[30m1　', '[34m2　', '[32m3　', '[33m4　', '[35m5弱', '[35m5強', '[31m6弱', '[31m6強', '[37;1;41m7　'];
-    const quake = rawQuake.filter(info => scale[info.earthquake.maxScale] != undefined && info.earthquake.hypocenter.name !== '' && info.earthquake.hypocenter.depth != -1 && info.earthquake.hypocenter.magnitude != -1);
+    const quake = rawQuakeInfo.filter(info => info.earthquake.maxScale !== -1 && info.earthquake.hypocenter.name !== '' && info.earthquake.hypocenter.depth !== -1 && info.earthquake.hypocenter.magnitude !== -1);
     const embed = new EmbedBuilder()
       .setTitle('地震情報')
       .setColor('#58b058')
@@ -25,7 +23,7 @@ module.exports = {
     for (let n = 0; n < 25; n++) {
       const date = new Date(quake[n].earthquake.time)
       const time = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
-      arr.push(`震度:${scale[quake[n].earthquake.maxScale]}|${quake[n].earthquake.hypocenter.name.padEnd(8, '　')}[0m|${time}|${quake[n].earthquake.hypocenter.depth.toString().padEnd(2, ' ')}km|M${quake[n].earthquake.hypocenter.magnitude}`)
+      arr.push(`震度:${quakeScales[quake[n].earthquake.maxScale].ansiColor + quakeScales[quake[n].earthquake.maxScale].name}|${quake[n].earthquake.hypocenter.name.padEnd(8, '　')}[0m|${time}|${quake[n].earthquake.hypocenter.depth.toString().padEnd(2, ' ')}km|M${quake[n].earthquake.hypocenter.magnitude}`)
     }
     arr.push('\n```')
 
