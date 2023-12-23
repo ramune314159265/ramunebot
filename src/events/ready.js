478f9db0309671d009ce554b489f8358ff2686ee
BuildTools <unconfigured@null.spigotmc.org>
2023-12-23 23:39:31 +0900

    featuresをready後に読み込むように

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,15 @@
 const {
     Events
 } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
 
 module.exports.name = Events.ClientReady
 module.exports.execute = () => {
     console.log('logged in')
     require('../interactions/index')
+
+    fs.readdirSync(path.join(__dirname, '../features'))
+        .filter(file => file.endsWith('.js'))
+        .forEach(file => require(path.join(__dirname, `../features/${file}`)))
 }
