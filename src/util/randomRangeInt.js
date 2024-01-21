5117be6f61912b8cc07f45a0dc5bb41344da326a
Ramune <121664026+ramune314159265@users.noreply.github.com>
2024-01-21 12:47:33 +0900

    修正

--- src/util/randomRangeInt.js
+++ src/util/randomRangeInt.js
@@ -1,5 +1,9 @@
 const randomRangeInt = (min, max) => {
+    return Math.round(Math.random() * ( max - min ) + min);
+}
+const randomRangeNum = (min, max) => {
     return Math.random() * ( max - min ) + min;
 }
 
-module.exports = randomRangeInt
+module.exports.randomRangeInt = randomRangeInt
+module.exports.randomRangeNum = randomRangeNum
