d4d624078fdff3444bbf11b2bc31bb885fb6af86
Ramune <121664026+ramune314159265@users.noreply.github.com>
2024-01-21 18:34:45 +0900

    スペースも対象に

--- src/util/toHankaku.js
+++ src/util/toHankaku.js
@@ -1,5 +1,7 @@
 const toHankakuAlphabet = string => {
-    return string.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
-        return String.fromCharCode(s.charCodeAt(0) - 0xFEE0)
-    })
+    return string
+        .replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
+            return String.fromCharCode(s.charCodeAt(0) - 0xFEE0)
+        })
+        .replaceAll("　"," ")
 }
