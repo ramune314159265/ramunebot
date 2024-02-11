30119c95003a2cabeb18d998b0e36a0e731061ef
Ramune <121664026+ramune314159265@users.noreply.github.com>
2024-02-11 17:12:08 +0900

    インデントをタブに

--- src/util/truncate.js
+++ src/util/truncate.js
@@ -10,13 +10,13 @@
  * @returns {string} - 結果が戻る
  */
 const truncate = (string, maxLength, ellipsis) => {
-    if (string.length <= maxLength) {
-        return string
-    }
-    const subString = string.slice(0, maxLength - 1)
-    return (ellipsis
-        ? subString.slice(0, subString.lastIndexOf(' '))
-        : subString) + '…'
+	if (string.length <= maxLength) {
+		return string
+	}
+	const subString = string.slice(0, maxLength - 1)
+	return (ellipsis
+		? subString.slice(0, subString.lastIndexOf(' '))
+		: subString) + '…'
 }
 
 module.exports.truncate = truncate
