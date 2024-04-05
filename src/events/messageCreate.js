d3c7b28387de9a7afbbcc65973d6eb35566db6e8
ramune314159265 <ramune314159265@gmail.com>
2024-04-05 23:10:12 +0900

    能力値まで追加

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,11 +1,16 @@
 const {
 	Events,
-	messageLink
+	messageLink,
+	EmbedBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle
 } = require('discord.js')
 
 const { DynamicLoader } = require('bcdice')
 const { createLocalStorage } = require('localstorage-ponyfill')
 const { toHankakuAlphabet } = require('../util/toHankaku')
+const { fetchIacharaData } = require('../util/iachara')
 const mainLoader = new DynamicLoader()
 let defaultGameSystem
 (async () => {
@@ -71,5 +76,53 @@ module.exports.execute = async message => {
 				replyMsg.delete()
 			}, 5000)
 		}
+	}
+	if (/https:\/\/iachara.com\/view\/(?:\d{1,8})/.test(message.content)) {
+		const regexpResult = /https:\/\/iachara.com\/view\/(?<id>\d{1,8})/.exec(message.content)
+		if (regexpResult === null) {
+			return
+		}
+		const id = regexpResult.groups.id
+		const characterData = await fetchIacharaData(id)
+		console.log(characterData)
+		if (!characterData.success) {
+			return
+		}
+
+		const embed = new EmbedBuilder()
+			.setAuthor({
+				name: 'いあきゃら',
+				iconURL: 'https://iachara.com/img/favicon.ico'
+			})
+			.setTitle(`${characterData.data.data.profile.name} (${characterData.data.data.profile.age}歳)`)
+			.setDescription(`
+職業…${characterData.data.data.profile.profession}
+身長…${characterData.data.data.profile.height}　体重…${characterData.data.data.profile.weight}
+性別…${characterData.data.data.profile.sex}
+出身…${characterData.data.data.profile.from}
+髪…${characterData.data.data.profile.hairColor}　瞳…${characterData.data.data.profile.eyeColor}　肌…${characterData.data.data.profile.skinColor}
+
+**能力値**
+STR ${String(characterData.data.data.abilities.str.value).padStart(' ')}　CON ${String(characterData.data.data.abilities.con.value).padStart(' ')}　POW ${String(characterData.data.data.abilities.pow.value).padStart(' ')}　DEX ${String(characterData.data.data.abilities.dex.value).padStart(' ')}
+APP ${String(characterData.data.data.abilities.app.value).padStart(' ')}　SIZ ${String(characterData.data.data.abilities.siz.value).padStart(' ')}　INT ${String(characterData.data.data.abilities.int.value).padStart(' ')}　EDU ${String(characterData.data.data.abilities.edu.value).padStart(' ')}
+`.trim()
+			)
+			.setURL(`https://iachara.com/view/${id}`)
+			.setColor('#1eff02')
+			.setImage(characterData.data.data.profile.icons[0].url)
+
+		message.channel.send({
+			content: '',
+			embeds: [embed],
+			components: [
+				new ActionRowBuilder().addComponents(
+					new ButtonBuilder()
+						.setStyle(ButtonStyle.Link)
+						.setLabel(`iachara.com/view/${id}`)
+						.setURL(`https://iachara.com/view/${id}`)
+				)
+			]
+		})
+		message.suppressEmbeds(true)
 	}
 }
