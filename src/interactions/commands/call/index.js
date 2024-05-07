ef6d86bda668710943f0958fbc4f67be4b3a30c8
ramune314159265 <ramune314159265@gmail.com>
2024-05-07 21:52:52 +0900

    修正

--- src/interactions/commands/call/index.js
+++ src/interactions/commands/call/index.js
@@ -8,7 +8,10 @@ module.exports = {
 		description: '通話に招待します',
 	},
 	async execute(interaction) {
-		if (interaction.guildId != '930376081196875787') await interaction.reply({ content: 'このコマンドはパーティークラフター専用です', ephemeral: true })
+		if (interaction.guildId != '930376081196875787') {
+			await interaction.reply({ content: 'ここでは利用できません', ephemeral: true })
+			return
+		}
 		const message = await interaction.guild.systemChannel
 			.send(`<@&1034812075635126332> ${userMention(interaction.user.id)}が https://discord.com/channels/930376081196875787/930819830821556294 に招待しています`)
 		await interaction.reply({ content: '招待を送信します', ephemeral: true })
