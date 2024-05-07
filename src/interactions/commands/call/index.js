5353d1b3ab884a5a21ec695d31f8831ce855fca3
ramune314159265 <ramune314159265@gmail.com>
2024-05-07 22:14:21 +0900

    ロールを指定できるように

--- src/interactions/commands/call/index.js
+++ src/interactions/commands/call/index.js
@@ -1,6 +1,7 @@
 const {
 	userMention,
-	ApplicationCommandOptionType
+	ApplicationCommandOptionType,
+	roleMention
 } = require('discord.js')
 
 module.exports = {
@@ -13,10 +14,14 @@ module.exports = {
 			name: 'invitechannel',
 			description: '招待する通話チャンネルを指定します',
 			required: false
+		}, {
+			type: ApplicationCommandOptionType.Role,
+			name: 'targetrole',
+			description: '招待する対象のロールを指定します',
+			required: false
 		}]
 	},
 	async execute(interaction) {
-		console.log(interaction.guild.channels.cache.find(channel => channel.isVoiceBased()))
 		const inviteChannel = interaction.options.getChannel('invitechannel') ?? interaction.guild.channels.cache.find(channel => channel.isVoiceBased())
 		if (!inviteChannel) {
 			await interaction.reply({ content: 'ボイスチャンネルが存在しません', ephemeral: true })
@@ -26,8 +31,9 @@ module.exports = {
 			await interaction.reply({ content: '指定したチャンネルはボイスチャンネルではありません', ephemeral: true })
 			return
 		}
+		const targetRole = interaction.options.getRole('targetrole').id ?? '1034812075635126332'
 		const message = await interaction.guild.systemChannel
-			.send(`<@&1034812075635126332> ${userMention(interaction.user.id)}が https://discord.com/channels/${interaction.guild.id}/${inviteChannel.id} に招待しています`)
+			.send(`${roleMention(targetRole)} ${userMention(interaction.user.id)}が https://discord.com/channels/${interaction.guild.id}/${inviteChannel.id} に招待しています`)
 		await interaction.reply({ content: '招待を送信します', ephemeral: true })
 		await message.react('982830355222515742')
 	}
