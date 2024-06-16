46eba789f00fbe64ac10fb1768080a028a5a0f85
ramune314159265 <ramune314159265@gmail.com>
2024-06-16 14:04:50 +0900

    戻した

--- src/util/getYoutubeResource.js
+++ src/util/getYoutubeResource.js
@@ -1,7 +1,7 @@
 const { createAudioResource,
 	StreamType
 } = require('@discordjs/voice')
-const { utils, videoInfo, getFormats, getReadableStream } = require('youtube-ext')
+const ytdl = require('ytdl-core')
 
 /**
  * 動画のAudioResourceを返す
@@ -9,17 +9,19 @@ const { utils, videoInfo, getFormats, getReadableStream } = require('youtube-ext
  * @param {string} videoId -Youtubeの動画ID
  * @returns {AudioResource}
  */
-const getResource = async videoId => {
-	const info = await videoInfo(videoId)
-	const formats = (await getFormats(info.stream))
-		.filter((format) => {
-			if (!format.url) return false
-			if (info.isLive) return utils.isHlsContentURL(format.url) && format.url.endsWith('.m3u8')
-			return typeof format.bitrate === 'number'
-		})
-		.sort((a, b) => Number(b.bitrate) - Number(a.bitrate))
-	const format = formats.find((format) => !format.qualityLabel) || formats.sort((a, b) => Number(a.bitrate) - Number(b.bitrate))[0]
-	const stream = await getReadableStream(format)
+const getResource = videoId => {
+	const stream = ytdl(videoId, {
+		filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus
+		highWaterMark: 32 * 1024 * 1024, // https://github.com/fent/node-ytdl-core/issues/902
+		quality: 'lowestaudio',
+		requestOptions: {
+			headers: {
+				...(process.env.YT_COOKIE && {
+					cookie: process.env.YT_COOKIE
+				})
+			}
+		}
+	})
 	const resource = createAudioResource(stream, {
 		inputType: StreamType.WebmOpus,
 		inlineVolume: true,
