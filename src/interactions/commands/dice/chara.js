3b85b8ab218cec85dfa7bf9a61f6d6fbdf6d2f02
ramune314159265 <ramune314159265@gmail.com>
2024-07-07 00:21:13 +0900

    カスタムダイス実装

--- src/interactions/commands/dice/chara.js
+++ src/interactions/commands/dice/chara.js
@@ -2,7 +2,12 @@ const { createLocalStorage } = require('localstorage-ponyfill')
 const {
 	ActionRowBuilder,
 	StringSelectMenuBuilder,
-	escapeItalic
+	escapeItalic,
+	ButtonBuilder,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+	ButtonStyle
 } = require('discord.js')
 const { sendAsUser } = require('../../../util/asUser')
 const { DynamicLoader } = require('bcdice')
@@ -53,7 +58,15 @@ module.exports.execute = async interaction => {
 	const message = await interaction.reply({
 		content: `${charaData.name} として振るダイスを選択してください`,
 		ephemeral: true,
-		components: returnCharaList(charaData, timeStamp)
+		components: [
+			...returnCharaList(charaData, timeStamp),
+			new ActionRowBuilder()
+				.addComponents(new ButtonBuilder()
+					.setCustomId('customDice')
+					.setLabel('カスタムダイス')
+					.setStyle(ButtonStyle.Primary)
+				)
+		]
 	})
 
 	const defaultGameSystem = await mainLoader.dynamicLoad('Cthulhu')
@@ -61,25 +74,81 @@ module.exports.execute = async interaction => {
 	const collector = await message.createMessageComponentCollector({ time: 6 * 60 * 60 * 1000 /*6時間*/ })
 
 	collector.on('collect', async collectorInteraction => {
-		userLastInteraction[collectorInteraction.user.id].deleteReply?.()
-		const interactionTimeStamp = collectorInteraction.customId.split('.')[0]
-		if (timeStamp !== interactionTimeStamp) {
-			return
+		switch (collectorInteraction.customId) {
+			case 'customDice': {
+				const diceCommandInput = new TextInputBuilder()
+					.setCustomId('diceCommand')
+					.setLabel('ダイスコマンドを入力')
+					.setStyle(TextInputStyle.Short)
+					.setPlaceholder('例: 3d6+1d4')
+					.setMaxLength(64)
+					.setRequired(true)
+				const modal = new ModalBuilder()
+					.setCustomId('customDiceModal')
+					.setTitle('カスタムダイス')
+					.addComponents(new ActionRowBuilder()
+						.addComponents(diceCommandInput)
+					)
+
+				await collectorInteraction.showModal(modal)
+				const filter = (i) => i.customId === 'customDiceModal'
+				const modalInteraction = await collectorInteraction.awaitModalSubmit({ filter, time: 1 * 60 * 1000 })
+				const diceCommand = modalInteraction.fields.getTextInputValue('diceCommand')
+				try {
+					const result = defaultGameSystem.eval(diceCommand).text
+					await modalInteraction.reply({
+						content: '送信します',
+						ephemeral: true
+					})
+					await sendAsUser({
+						message: {
+							content: escapeItalic(`${diceCommand} ${result}`)
+						},
+						avatar: charaData.iconUrl,
+						name: `${truncate(charaData.name, 12)} / ${collectorInteraction.member.displayName}`,
+						channel: collectorInteraction.channel
+					})
+				} catch (e) {
+					modalInteraction.reply({
+						content: 'エラーが発生しました。コマンドが間違っている又はサーバーエラーの可能性があります。',
+						ephemeral: true
+					})
+				}
+
+				break
+			}
+
+			default: {
+				userLastInteraction[collectorInteraction.user.id].deleteReply?.()
+				const interactionTimeStamp = collectorInteraction.customId.split('.')[0]
+				if (timeStamp !== interactionTimeStamp) {
+					return
+				}
+				const diceCommand = collectorInteraction.values[0]
+				await sendAsUser({
+					message: {
+						content: escapeItalic(`${diceCommand} ${defaultGameSystem.eval(diceCommand).text}`)
+					},
+					avatar: charaData.iconUrl,
+					name: `${truncate(charaData.name, 12)} / ${collectorInteraction.member.displayName}`,
+					channel: collectorInteraction.channel
+				})
+				userLastInteraction[collectorInteraction.user.id] = collectorInteraction
+				await collectorInteraction.reply({
+					content: `${charaData.name} として振るダイスを選択してください`,
+					ephemeral: true,
+					components: [
+						...returnCharaList(charaData, timeStamp),
+						new ActionRowBuilder()
+							.addComponents(new ButtonBuilder()
+								.setCustomId('customDice')
+								.setLabel('カスタムダイス')
+								.setStyle(ButtonStyle.Primary)
+							)
+					]
+				})
+				break
+			}
 		}
-		const diceCommand = collectorInteraction.values[0]
-		await sendAsUser({
-			message: {
-				content: escapeItalic(`${diceCommand} ${defaultGameSystem.eval(diceCommand).text}`)
-			},
-			avatar: charaData.iconUrl,
-			name: `${truncate(charaData.name, 12)} / ${collectorInteraction.member.displayName}`,
-			channel: collectorInteraction.channel
-		})
-		userLastInteraction[collectorInteraction.user.id] = collectorInteraction
-		await collectorInteraction.reply({
-			content: `${charaData.name} として振るダイスを選択してください`,
-			ephemeral: true,
-			components: returnCharaList(charaData, timeStamp)
-		})
 	})
 }
